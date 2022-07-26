a53c68ef1598562a2479e16c1fa5608074058c99
Jack Ross <jack-ross-iii@pm.me>
2022-07-26 15:21:11 -0400

    [#4] Terraform: Adding logging to the Lambda function

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -1,5 +1,6 @@
 locals {
   prefix = "${var.project_name}-${var.unique_identifier}"
+  lambda_name = "${local.prefix}-lambda"
 }
 
 data "archive_file" "zip_lambda_code" {
@@ -28,10 +29,48 @@ resource "aws_iam_role" "lambda_role" {
 EOF
 }
 
+resource "aws_iam_policy" "lambda_logging" {
+  name = "${local.prefix}-lambda-logging"
+  path = "/"
+  description = "Policy to allow the IAM role for Lambda to write to CloudWatch Logs"
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": [
+        "logs:CreateLogGroup",
+        "logs:CreateLogStream",
+        "logs:PutLogEvents"
+      ],
+      "Resource": "arn:aws:logs:*:*:*",
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_logging" {
+  role = aws_iam_role.lambda_role.name
+  policy_arn = aws_iam_policy.lambda_logging.arn
+}
+
+resource "aws_cloudwatch_log_group" "lambda_log_group" {
+  name = "/aws/lambda/${local.lambda_name}"
+  retention_in_days = 14
+}
+
 resource "aws_lambda_function" "lambda_function" {
   filename = "${var.working_dir}/lambda.zip"
-  function_name = "${local.prefix}-lambda"
+  function_name = "${local.lambda_name}"
   handler = "index.handler"
   runtime = "nodejs16.x"
   role = aws_iam_role.lambda_role.arn
+
+  depends_on = [
+    aws_cloudwatch_log_group.lambda_log_group,
+    aws_iam_role_policy_attachment.lambda_logging
+  ]
 }
