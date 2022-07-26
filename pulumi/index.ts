bda8af6162496368edf3b808977b710de0804d7e
Jack Ross <jack-ross-iii@pm.me>
2022-07-26 13:02:48 -0400

    Removing the default S3 bucket that's created with the Pulumi template

--- pulumi/index.ts
+++ pulumi/index.ts
@@ -2,14 +2,10 @@ import * as pulumi from "@pulumi/pulumi";
 import * as aws from "@pulumi/aws";
 import * as AdmZip from 'adm-zip';
 
-
 const config = new pulumi.Config();
 const projectName = config.require("project_name");
 const uniqueId = config.require("unique_identifier");
 const prefix = `${projectName}-${uniqueId}`
-
-// Create an AWS resource (S3 Bucket)
-const bucket = new aws.s3.Bucket("pulumi-created-bucket");
 
 const lambdaRole = new aws.iam.Role(`lambdaRole`, {
   name: `${prefix}-lambda`,
@@ -33,14 +29,9 @@ const lambdaZip = new AdmZip();
 lambdaZip.addLocalFolder("../lambda");
 lambdaZip.writeZip("lambda.zip");
 
-/*
 const lambdaFunction = new aws.lambda.Function(`${prefix}-lambda`, {
   code: new pulumi.asset.FileArchive("lambda.zip"),
   role: lambdaRole.arn,
   handler: "index.handler",
   runtime: "nodejs16.x",
 });
-*/
-
-// Export the name of the bucket
-export const bucketName = bucket.id;
