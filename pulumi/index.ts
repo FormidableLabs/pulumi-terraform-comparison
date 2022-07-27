23c3f07cd562514a59cedd1a7dc6444ed81f9357
Jack Ross <jack-ross-iii@pm.me>
2022-07-27 12:23:34 -0400

    [#2] Pulumi: Adding additional API Gateway resources. Currently receiving an error however

--- pulumi/index.ts
+++ pulumi/index.ts
@@ -73,6 +73,52 @@ const lambdaFunction = new aws.lambda.Function(`${lambdaName}`, {
   ],
 });
 
-const apiGateway = new aws.apigatewayv2.Api("${prefix}-api-gateway", {
+
+const apiGateway = new aws.apigatewayv2.Api(`${prefix}-api-gateway`, {
   protocolType: "HTTP",
 });
+
+const lambdaPermission = new aws.lambda.Permission(`${prefix}-lambda-permission`, {
+  statementId: "AllowExecutionFromAPIGateway",
+  action: "lambda:InvokeFunction",
+  principal: "apigateway.amazonaws.com",
+  function: lambdaFunction,
+  sourceArn: pulumi.interpolate`${apiGateway.executionArn}/*/*`,
+}, {dependsOn: [apiGateway, lambdaFunction]});
+
+const apiIntegration = new aws.apigatewayv2.Integration(`${prefix}-api-integration`, {
+  apiId: apiGateway.id,
+  integrationUri: lambdaFunction.invokeArn,
+  integrationType: "AWS_PROXY",
+  integrationMethod: "POST",
+});
+
+const apiRoute = new aws.apigatewayv2.Route(`${prefix}-api-route`, {
+  apiId: apiGateway.id,
+  routeKey: "GET /hello",
+  target: pulumi.interpolate`integrations/${apiIntegration.id}"`,
+});
+
+const gatewayStage = new aws.apigatewayv2.Stage(`${prefix}-api-stage`, {
+  apiId: apiGateway.id,
+  name: `${prefix}-api-stage`,
+  autoDeploy: true,
+  accessLogSettings: {
+    destinationArn: cloudWatch.arn,
+    format: JSON.stringify({
+      requestId               : "$context.requestId",
+      sourceIp                : "$context.identity.sourceIp",
+      requestTime             : "$context.requestTime",
+      protocol                : "$context.protocol",
+      httpMethod              : "$context.httpMethod",
+      resourcePath            : "$context.resourcePath",
+      routeKey                : "$context.routeKey",
+      status                  : "$context.status",
+      responseLength          : "$context.responseLength",
+      integrationErrorMessage : "$context.integrationErrorMessage",
+    })
+  }
+}, {dependsOn: [apiRoute]});
+
+
+export const endpointUrl = pulumi.interpolate`${gatewayStage.invokeUrl}`;
