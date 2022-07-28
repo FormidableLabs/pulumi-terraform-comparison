935dc0ecca0a61f75a6b50ab31f5a2e32a95d738
Jack Ross <jack-ross-iii@pm.me>
2022-07-28 12:10:33 -0400

    [#7] Pulumi: Moving Lambda permissions into separate file

--- pulumi/index.ts
+++ pulumi/index.ts
@@ -4,6 +4,7 @@ import * as aws from "@pulumi/aws";
 import * as apiGatewayImport from "./api-gateway";
 import * as cloudFrontImport from "./cloudfront";
 import * as lambdaCode from "./lambdaCode";
+import * as lambdaPermissions from "./lambdaPermissions";
 
 const config = new pulumi.Config();
 const projectName = config.require("project_name");
@@ -12,47 +13,7 @@ const prefix = `${projectName}-${uniqueId}`;
 const lambdaName = `${prefix}-lambda`;
 const lambdaZipName = "lambda.zip";
 
-const lambdaLoggingPolicy = new aws.iam.Policy(`${prefix}-lambda-logging`, {
-  path: "/",
-  description: "Policy to allow the IAM role for Lambda to write to CloudWatch Logs",
-  policy: `{
-    "Version": "2012-10-17",
-    "Statement": [
-      {
-        "Action": [
-          "logs:CreateLogGroup",
-          "logs:CreateLogStream",
-          "logs:PutLogEvents"
-        ],
-        "Resource": "arn:aws:logs:*:*:*",
-        "Effect": "Allow",
-        "Sid": ""
-      }
-    ]
-  }`,
-});
-
-const lambdaRole = new aws.iam.Role(`${prefix}-lambda`, {
-  name: `${prefix}-lambda`,
-  assumeRolePolicy: `{
-    "Version": "2012-10-17",
-    "Statement": [
-      {
-        "Action": "sts:AssumeRole",
-        "Principal": {
-          "Service": "lambda.amazonaws.com"
-        },
-        "Effect": "Allow",
-        "Sid": ""
-      }
-    ]
-  }`
-});
-
-const lambdaRoleAttachment = new aws.iam.RolePolicyAttachment(`${prefix}-logs`, {
-  role: lambdaRole.name,
-  policyArn: lambdaLoggingPolicy.arn,
-});
+const { lambdaRole, lambdaRoleAttachment } = lambdaPermissions.createLambdaPermissions(prefix);
 
 const cloudWatch = new aws.cloudwatch.LogGroup(`/aws/lambda/${lambdaName}`, {
   name: `/aws/lambda/${lambdaName}`,
---
+++ pulumi/lambdaPermissions.ts
@@ -0,0 +1,47 @@
+import * as aws from "@pulumi/aws";
+
+export function createLambdaPermissions(prefix: string) {
+  const lambdaLoggingPolicy = new aws.iam.Policy(`${prefix}-lambda-logging`, {
+    path: "/",
+    description: "Policy to allow the IAM role for Lambda to write to CloudWatch Logs",
+    policy: `{
+      "Version": "2012-10-17",
+      "Statement": [
+        {
+          "Action": [
+            "logs:CreateLogGroup",
+            "logs:CreateLogStream",
+            "logs:PutLogEvents"
+          ],
+          "Resource": "arn:aws:logs:*:*:*",
+          "Effect": "Allow",
+          "Sid": ""
+        }
+      ]
+    }`,
+  });
+
+  const lambdaRole = new aws.iam.Role(`${prefix}-lambda`, {
+    name: `${prefix}-lambda`,
+    assumeRolePolicy: `{
+      "Version": "2012-10-17",
+      "Statement": [
+        {
+          "Action": "sts:AssumeRole",
+          "Principal": {
+            "Service": "lambda.amazonaws.com"
+          },
+          "Effect": "Allow",
+          "Sid": ""
+        }
+      ]
+    }`
+  });
+
+  const lambdaRoleAttachment = new aws.iam.RolePolicyAttachment(`${prefix}-logs`, {
+    role: lambdaRole.name,
+    policyArn: lambdaLoggingPolicy.arn,
+  });
+
+  return { lambdaRole, lambdaRoleAttachment };
+}
