1514d69cb19331292faf90be126db2c3d4d8c253
Jack Ross <jack-ross-iii@pm.me>
2022-07-28 11:17:58 -0400

    [#5] Terraform: Updating outputs to also include domain and full URL
    Tested that modifying the Lambda code does redeploy the changes when
    there are changes (and doesn't when there aren't any changes)

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,9 +1,9 @@
 output "api_gateway_url" {
   description = "The Base URL for the API Gateway"
-  value = aws_apigatewayv2_stage.gateway_stage.invoke_url
+  value = "${aws_apigatewayv2_stage.gateway_stage.invoke_url}/hello"
 }
 
 output "cloudfront_url" {
   description = "The base URL for the CloudFront distro"
-  value = aws_cloudfront_distribution.api_gateway.domain_name
+  value = "https://${aws_cloudfront_distribution.api_gateway.domain_name}/hello"
 }
