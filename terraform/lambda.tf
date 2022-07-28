27cac84bc76f89ec76851f778eea26b240e5e025
Jack Ross <jack-ross-iii@pm.me>
2022-07-28 10:39:27 -0400

    [#5] Terraform: Updated to upload Lambda code zip file to S3 rather than referencing locally

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -3,10 +3,26 @@ locals {
   lambda_name = "${local.prefix}-lambda"
 }
 
+resource "aws_s3_bucket" "code_bucket" {
+  bucket = "${local.prefix}-code-bucket"
+}
+
+resource "aws_s3_bucket_acl" "code_bucket_private" {
+  acl = "private"
+  bucket = aws_s3_bucket.code_bucket.id
+}
+
 data "archive_file" "zip_lambda_code" {
   type = "zip"
   source_file = "${var.working_dir}/../lambda/index.js"
   output_path = "${var.working_dir}/lambda.zip"
+}
+
+resource "aws_s3_object" "lambda_zip" {
+  bucket = aws_s3_bucket.code_bucket.id
+  key = "lambda.zip"
+  source = data.archive_file.zip_lambda_code.output_path
+  etag = filemd5(data.archive_file.zip_lambda_code.output_path)
 }
 
 resource "aws_iam_role" "lambda_role" {
@@ -63,8 +79,12 @@ resource "aws_cloudwatch_log_group" "lambda_log_group" {
 }
 
 resource "aws_lambda_function" "lambda_function" {
-  filename = "${var.working_dir}/lambda.zip"
   function_name = "${local.lambda_name}"
+
+  s3_bucket = aws_s3_bucket.code_bucket.id
+  s3_key = aws_s3_object.lambda_zip.key
+  source_code_hash = data.archive_file.zip_lambda_code.output_base64sha256
+
   handler = "index.handler"
   runtime = "nodejs16.x"
   role = aws_iam_role.lambda_role.arn
